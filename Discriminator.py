c858b24f8028e890f518a76c261138feaec0e92a
GengXieBo <1181895140@qq.com>
2019-07-01 11:14:42 +0800

    Add files via upload

---
+++ Discriminator.py
@@ -0,0 +1,48 @@
+import torch.nn.functional as F
+from torch import nn
+
+class DiscriminatorPatch64(nn.Module):
+    def __init__(self):
+        super(DiscriminatorPatch64, self).__init__()
+        self.net = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, padding=1),
+            nn.LeakyReLU(0.2),
+
+            nn.Conv2d(64, 64, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(64),
+            nn.LeakyReLU(0.2),
+
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.2),
+
+            nn.Conv2d(128, 128, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.2),
+
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.2),
+
+            nn.Conv2d(256, 256, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.2),
+
+            nn.Conv2d(256, 512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.2),
+
+            nn.Conv2d(512, 512, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.2),
+
+            nn.MaxPool2d(kernel_size=(4, 4), stride=(4, 4)),
+
+            nn.Conv2d(512, 1, kernel_size=1)
+        )
+
+    def forward(self, x):
+        output = self.net(x)
+        output_shape = (output.size(0), output.size(2), output.size(3))
+        final_output = F.sigmoid(output.view(output_shape))
+        return final_output
